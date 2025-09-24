731e96d6b2bb9074daa18688422c7660e76eb98e
Sankeerth Ajith <sankeerthajith3@gmail.com>
2025-09-25 01:00:41 +0530

    type error fix

--- client/my-app/src/components/inputbox.tsx
+++ client/my-app/src/components/inputbox.tsx
@@ -1,23 +1,29 @@
 import { useState } from "react";
 
-const InputBar = ({ currentMessage, setCurrentMessage, onSubmit }) => {
-  const handleChange = (e) => {
+interface InputBarProps {
+  currentMessage: string;
+  setCurrentMessage: React.Dispatch<React.SetStateAction<string>>;
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+const InputBar: React.FC<InputBarProps> = ({ currentMessage, setCurrentMessage, onSubmit }) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCurrentMessage(e.target.value);
   };
+
   return (
-    <form onSubmit={onSubmit} className=" ">
-      <div className="flex items-center  p-3 px-6 justify-between  retro ">
+    <form onSubmit={onSubmit} className="">
+      <div className="flex items-center p-3 px-6 justify-between retro">
         <input
           type="text"
           placeholder="TYPE HERE"
           value={currentMessage}
           onChange={handleChange}
-          className="flex-grow px-2 py-2  focus:outline-none text-black font-bold"
+          className="flex-grow px-2 py-2 focus:outline-none text-black font-bold"
         />
-
         <button
           type="submit"
-          className=" text-black border-4  px-3 py-2 ml-2 bg-white hover: transition-all duration-200 group retroPop"
+          className="text-black border-4 px-3 py-2 ml-2 bg-white hover:transition-all duration-200 group retroPop"
         >
           send
         </button>
@@ -25,4 +31,5 @@ const InputBar = ({ currentMessage, setCurrentMessage, onSubmit }) => {
     </form>
   );
 };
+
 export default InputBar;
